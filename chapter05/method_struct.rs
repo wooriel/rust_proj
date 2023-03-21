c0bf6a50e966accfbfa615f94577b9eb42587b17
riella <wooriel@gmail.com>
2023-03-21 16:11:24 +0900

    finished chapter05

--- chapter05/method_struct.rs
+++ chapter05/method_struct.rs
@@ -5,19 +5,55 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    // fn width(&self) -> bool {
+    //     self.width > 0
+    // }
+
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
 fn main() {
     let rect1 = Rectangle {
-        width: 50,
-        height: 30,
+        width: 30,
+        height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let square1 = Rectangle::square(3);
 
     println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
+        "Can rect1 hold rect2? {}",
+        rect1.can_hold(&rect2)
+    );
+    println!(
+        "Can rect1 hold rect3? {}",
+        rect1.can_hold(&rect3)
+    );
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     rect1.area()
+    // );
+    println!(
+        "The area of the square is {} square pixels.",
+        square1.area()
     );
 }
